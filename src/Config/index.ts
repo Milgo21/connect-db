797d3773ce78fcc62150900f582c53568ab2ce43
Milgo21 <felixmilgo21@gmail.com>
2023-02-08 07:34:39 +0300

    Connected successfully

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -2,10 +2,10 @@ import dotenv from 'dotenv'
 import path from 'path'
 dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 import mssql from 'mssql'
-const sqlConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
+export const sqlConfig = {
+    user:process.env.DB_USER,
+    password:process.env.DB_PWD,
+    database:process.env.DB_NAME,
     server: 'localhost',
 
 pool: {
@@ -18,13 +18,14 @@ options:{
     trustServerCertificate: false // change to true for local dev / self-signed certs  
   }
 }
-  console.log('Running');
+console.log('Running');
 
 
 
 const checkConnection = async ()=>{
-try {const x= await mssql.connect(sqlConfig)
-if(x.connected){
+try {
+  const x = await mssql.connect(sqlConfig)
+  if(x.connected){
   console.log("Connected to the Database");
 } 
 } catch (error) {
